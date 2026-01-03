a2c8b32cc9df91ac5be8114c06ceb89e916b9076
Roopa Chiluvuri <roopasree2509@gmail.com>
2026-01-02 17:46:06 -0800

    Containers

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -5,16 +5,17 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
   let foodItems = ["Paneer", "Tofu", "Eggs", "Dal", "Lentils", "SoyBean"];
 
   return (
-    <>
+    <Container>
       <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage items={foodItems}></ErrorMessage>
       <FoodItems items={foodItems}></FoodItems>
-    </>
+    </Container>
   );
 }
 
